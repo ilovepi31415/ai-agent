a076bf668809cc058fb97e24b64e2df45a8929cf
David Randall <davidra@andrews.edu>
2025-09-17 09:48:16 -0400

    Implement get_file_content

---
+++ functions/get_file_content.py
@@ -0,0 +1,19 @@
+import os
+
+CHARACTER_LIMIT = 10000
+
+def get_file_content(working_directory, file_path):
+    try:
+        joined_path = os.path.join(working_directory, file_path)
+        print(joined_path)
+        if not os.path.abspath(joined_path).startswith(os.path.abspath(working_directory)):
+            return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+        if not os.path.isfile(joined_path):
+            return f'Error: file not found or is not a regular file'
+        f = open(joined_path)
+        contents = f.read()
+        if len(contents) > CHARACTER_LIMIT:
+            return contents[:CHARACTER_LIMIT] + f'[...File "{file_path}" truncated at {CHARACTER_LIMIT} characters]'
+        return contents
+    except Exception as e:
+        return f"Error: {e}"
--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -2,15 +2,15 @@ import os
 
 def get_files_info(working_directory, directory="."):
     try:
-        new_dir = os.path.join(working_directory, directory)
-        if not os.path.abspath(new_dir).startswith(os.path.abspath(working_directory)):
+        joined_dir = os.path.join(working_directory, directory)
+        if not os.path.abspath(joined_dir).startswith(os.path.abspath(working_directory)):
             return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
-        print(new_dir)
-        if not os.path.isdir(new_dir):
+        print(joined_dir)
+        if not os.path.isdir(joined_dir):
             return f'Error: "{directory}" is not a directory'
         file_string = ''
-        for path in os.listdir(new_dir):
-            true_path = os.path.join(new_dir, path)
+        for path in os.listdir(joined_dir):
+            true_path = os.path.join(joined_dir, path)
             file_string += f"- {path}: file_size={os.path.getsize(true_path)}, is_dir={os.path.isdir(true_path)}\n"
         return file_string
     except Exception as e:
